b66f70741a27fe56a47c9bdf5016ce3c34f52360
agc11 <adrian11gomez@gmail.com>
2015-11-17 14:12:57 +0100

    addList

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,25 +1,52 @@
 import React, { Component, PropTypes } from 'react';
 import List from '../components/List';
-import { Dialog } from 'material-ui';
+import { Dialog, TextField, FlatButton } from 'material-ui';
 export default class Section extends Component {
 
   constructor(props){
     super(props);
   }
 
+  validationTitle(title){
+    const { lists } = this.props;
+    const number = lists.filter( list => list.title===title);
+    return title!=='' && lists.filter( list => list.title===title).length===0;
+
+
+
+  }
+
   openDialog(){
     this.refs.dialog.show();
   }
 
+  onClickAdd(){
+    const { onAddList } = this.props;
+    const title = this.refs.titleDialog.getValue();
+    this.validationTitle(title) ? onAddList(title) : this.refs.dialog.dismiss();
+    this.refs.dialog.dismiss();
+  }
+
+  onClickClose(){
+    this.refs.dialog.dismiss();
+  }
+
   render() {
-    let standardActions = [
-      { text: 'Cancel' },
-      { text: 'Submit', onTouchTap: this._onDialogSubmit, ref: 'submit' }
+    let customActions = [
+      <FlatButton
+        label="Cancel"
+        secondary={true}
+        onClick={() => this.onClickClose()} />,
+      <FlatButton
+        label="Add"
+        primary={true}
+        onClick={() => this.onClickAdd()} />
     ];
     const {  lists, onEditList, onRemoveList } = this.props;
     return(
       <div className="article">
-      <Dialog title="Dialog With Standard Actions" actions={standardActions} ref="dialog">
+        <Dialog title="Dialog With Standard Actions" actions={customActions} ref="dialog">
+          <TextField ref="titleDialog" hintText="Title List" />
         </Dialog>
         <div className="lists">
             {
