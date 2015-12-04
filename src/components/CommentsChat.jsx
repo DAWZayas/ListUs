41d82183b8e4b21c90a57e6f81448b6c4b19f597
kuasi <adrian11gomez@gmail.com>
2015-12-04 20:12:37 +0100

    chat better

--- src/components/CommentsChat.jsx
+++ src/components/CommentsChat.jsx
@@ -32,12 +32,37 @@ export default class CommentsChat extends Component {
   changeDay(index){
     const {comments} = this.props;
     if(index===0){
-      return <div className="commentDate"><span>{comments[index].date===this.state.startDate ? this.state.startDate : comments[index].date}</span></div>;
+      return <div className="commentDate"><span>{comments[index].date===this.state.startDate ? 'HOY' : comments[index].date}</span></div>;
     }
     if(comments[index].date!==comments[index-1].date){
       return <div className="commentDate">{comments[index].date===this.state.startDate ? 'HOY' : comments[index].date}</div>;
     }
     return '';
+  }
+
+  haveBlankSpace(msg){
+    debugger;
+    for(let i = 0; msg.length>i; i++){
+      if(msg[i]===' '){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  formatMsg(msg){
+    let formatMsg = '';
+    if(this.haveBlankSpace(msg)){
+      formatMsg = msg;
+    }else{
+      for(let i = 0; msg.length>i; i++){
+        if(i%135===0){
+          formatMsg = formatMsg +'\n';
+        }
+        formatMsg = formatMsg + msg[i];
+      }
+    }
+    return formatMsg;
   }
 
   render(){
@@ -48,16 +73,16 @@ export default class CommentsChat extends Component {
         <div>
           <ul className="listComments">
             {
-              this.props.comments.map( (comment, index) =>
+              comments.map( (comment, index) =>
                   <div className=""key={index}>
                   {this.changeDay(index)}
-                  <li className="itemComment"><div className="commentMsg ">{comment.msg}</div><div className="commentInfo "><div className="commentHour">{comment.hour}</div><div className="commentUser">{comment.user}</div></div></li>
+                  <li className="itemComment"><div className="commentInfo "><div className="commentUser">{comment.user}</div><div className="commentMsg ">{this.formatMsg(comment.msg)}</div></div><div className="commentHour">{comment.hour}</div></li>
                   </div>)
             }
           </ul>
         </div>
         <div className="messageAndButtonSend">
-          <input className="form-control" ref="textArea" />
+          <textarea className="form-control inputSendMsg" ref="textArea" max-height="140"></textarea>
           <button className="btn btn-success glyphicon glyphicon-send buttonSendMessage" onClick={ () => this.onClickAddComment()}></button>
         </div>
     </div>
